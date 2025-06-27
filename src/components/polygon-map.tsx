3989040c25f51ee2609d6d97f62d62b6364f272e
Johann Beukes <johann@redblueconsulting.com>
2025-06-27 18:26:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/polygon-map.tsx
+++ src/components/polygon-map.tsx
@@ -28,8 +28,8 @@ export function PolygonMap({
     setDrawingManagerOptions({
       drawingControl: true,
       drawingControlOptions: {
-        position: maps.ControlPosition.TOP_CENTER,
-        drawingModes: [drawing.DrawingMode.POLYGON],
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.DrawingMode.POLYGON],
       },
       polygonOptions: {
         fillColor: 'hsl(var(--primary))',
